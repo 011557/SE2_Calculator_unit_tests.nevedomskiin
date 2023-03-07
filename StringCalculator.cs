9dbbd68015c24f7439cf13d33c2c0a589d8b16eb
011557 <01155789@pw.edu.pl>
2023-03-07 07:31:53 +0300

    Initial commit

---
+++ StringCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE2_Calculator_unit_tests
+{
+    class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == "") return 0;
+            else
+            {
+                string[] strArray = numbers.Split();
+                int a, b;
+                Int32.TryParse(strArray[0], out a);
+                Int32.TryParse(strArray[1], out b);
+                return a + b;
+            }
+        }
+    }
+}
